be8e63849d5f2153412a41be59852b3b500233aa
Alex M <milesAwayAlex@gmail.com>
2021-08-04 00:28:41 -0700

    Add Show

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import InterviewerList from 'components/InterviewerList';
 import Appointment from 'components/Appointment';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
+import Show from 'components/Appointment/Show';
 
 storiesOf('Button', module)
   .addParameters({
@@ -143,4 +144,12 @@ storiesOf('Appointment', module)
   .add('Appointment', () => <Appointment />)
   .add('With Time', () => <Appointment time="12pm" />)
   .add('Header', () => <Header time="12pm" />)
-  .add('Empty', () => <Empty onAdd={action('onAdd')} />);
+  .add('Empty', () => <Empty onAdd={action('onAdd')} />)
+  .add('Show', () => (
+    <Show
+      student="Lydia Miller"
+      interviewer={interviewer}
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+    />
+  ));
