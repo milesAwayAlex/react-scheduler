99dda6dd82b30376275ea47d5477b6000d2bfb0a
Alex M <milesAwayAlex@gmail.com>
2021-08-04 02:20:56 -0700

    Add appointment views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
-
 import 'components/Appointment/styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 export default function Appointment(props) {
-  return <article className="appointment">{props.children}</article>;
+  const i = props.interview;
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {i ? <Show student={i.student} interviewer={i.interviewer} /> : <Empty />}
+    </article>
+  );
 }
