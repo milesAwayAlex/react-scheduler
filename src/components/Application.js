c72e099f462e0d7136bbafd250d5163bfd33a6b0
Alex M <milesAwayAlex@gmail.com>
2021-08-04 02:33:08 -0700

    Lay out the schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,46 @@ import React, { useState } from 'react';
 
 import 'components/Application.scss';
 import DayList from './DayList';
+import Appointment from './Appointment';
+
+const appointments = [
+  {
+    id: 1,
+    time: '12pm',
+  },
+  {
+    id: 2,
+    time: '1pm',
+    interview: {
+      student: 'Lydia Miller-Jones',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 3,
+    time: '2pm',
+    interview: {
+      student: 'Whatever',
+      interviewer: {
+        id: 1,
+        name: 'Sylvia Palmer',
+        avatar: 'https://i.imgur.com/LpaY82x.png',
+      },
+    },
+  },
+  {
+    id: 4,
+    time: '3pm',
+  },
+  {
+    id: 5,
+    time: '4pm',
+  },
+];
 
 const days = [
   {
@@ -42,7 +82,11 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments
+          .map((appointment) => (
+            <Appointment key={appointment.id} {...appointment} />
+          ))
+          .concat(<Appointment key="last" time="5pm" />)}
       </section>
     </main>
   );
