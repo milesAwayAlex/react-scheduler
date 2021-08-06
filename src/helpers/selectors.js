859ce81cbc89a51c4e94ae6a45026a6d8599ba9b
Alex M <milesAwayAlex@gmail.com>
2021-08-05 18:56:05 -0700

    Add getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,3 +3,9 @@ export const getAppointmentsForDay = (state, dayName) => {
   if (!day) return [];
   return day.appointments.map((id) => state.appointments[id]);
 };
+
+export const getInterview = (state, iview) => {
+  if (!iview) return null;
+  const { student, interviewer } = iview;
+  return { student, interviewer: state.interviewers[interviewer] };
+};
