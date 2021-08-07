472d5c36d00a6dbcd722fcdf8e7c39d092bb63c5
Alex M <milesAwayAlex@gmail.com>
2021-08-07 00:11:51 -0700

    Switch over to useReducer

--- src/hooks/useAppData.js
+++ src/hooks/useAppData.js
@@ -1,83 +1,93 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 
 import 'components/Application.scss';
 import axios from 'axios';
 
 export default () => {
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    iws: {},
-    interviewers: [],
-  });
-  const setDay = (day) => setState({ ...state, day });
-  const day = state.days.find((d) => d.name === state.day);
-  const dayInd = state.days.findIndex((d) => d.name === state.day);
-  // console.log(spots);
+  const [state, dispatch] = useReducer(
+    (st, act) => {
+      const day = st.days.find((d) => d.name === st.day);
+      const re = {
+        setDay: (day) => ({ ...st, day }),
+        bookInterview: (id, interview) => {
+          const upSt = {
+            ...st,
+            appointments: {
+              ...st.appointments,
+              [id]: {
+                ...st.appointments[id],
+                interview: { ...interview },
+              },
+            },
+          };
+          return {
+            ...upSt,
+            days: [
+              ...upSt.days.map((d) => {
+                if (d.id !== day.id) return d;
+                return {
+                  ...d,
+                  spots:
+                    !!day &&
+                    Object.values(upSt.appointments)
+                      .filter((a) => d.appointments.includes(a.id))
+                      .filter((a) => a.interview === null).length,
+                };
+              }),
+            ],
+          };
+        },
+        deleteAppointment: (id) => {
+          const upSt = {
+            ...st,
+            appointments: {
+              ...st.appointments,
+              [id]: { ...st.appointments[id], interview: null },
+            },
+          };
+          return {
+            ...upSt,
+            days: [
+              ...upSt.days.map((d) => {
+                if (d.id !== day.id) return d;
+                return {
+                  ...d,
+                  spots:
+                    !!day &&
+                    Object.values(upSt.appointments)
+                      .filter((a) => d.appointments.includes(a.id))
+                      .filter((a) => a.interview === null).length,
+                };
+              }),
+            ],
+          };
+        },
+        fetch: (d, a, i) => ({
+          ...st,
+          days: d.data,
+          appointments: a.data,
+          interviewers: i.data,
+        }),
+      };
+      return re[act.type](...act.args);
+    },
+    {
+      day: 'Monday',
+      days: [],
+      appointments: {},
+      iws: {},
+      interviewers: [],
+    }
+  );
+  const setDay = (day) => dispatch({ type: 'setDay', args: [day] });
   const bookInterview = (id, interview) =>
     axios
       .put(`/api/appointments/${id}`, { interview: { ...interview } })
-      .then(() => {
-        setState((s) => ({
-          ...s,
-          appointments: {
-            ...s.appointments,
-            [id]: {
-              ...s.appointments[id],
-              interview: { ...interview },
-            },
-          },
-        }));
-      })
-      .then(() =>
-        setState((s) => ({
-          ...s,
-          days: [
-            ...s.days.map((d) => {
-              if (d.id !== day.id) return d;
-              return {
-                ...d,
-                spots:
-                  !!day &&
-                  Object.values(s.appointments)
-                    .filter((a) => d.appointments.includes(a.id))
-                    .filter((a) => a.interview === null).length,
-              };
-            }),
-          ],
-        }))
-      );
+      .then(() => dispatch({ type: 'bookInterview', args: [id, interview] }));
   const deleteAppointment = (id) =>
     axios
       .delete(`/api/appointments/${id}`)
-      .then(() =>
-        setState((s) => ({
-          ...s,
-          appointments: {
-            ...s.appointments,
-            [id]: { ...s.appointments[id], interview: null },
-          },
-        }))
-      )
-      .then(() =>
-        setState((s) => ({
-          ...s,
-          days: [
-            ...s.days.map((d) => {
-              if (d.id !== day.id) return d;
-              return {
-                ...d,
-                spots:
-                  !!day &&
-                  Object.values(s.appointments)
-                    .filter((a) => d.appointments.includes(a.id))
-                    .filter((a) => a.interview === null).length,
-              };
-            }),
-          ],
-        }))
-      );
+      .then(() => dispatch({ type: 'deleteAppointment', args: [id] }));
 
   useEffect(() => {
     Promise.all([
@@ -85,14 +95,7 @@ export default () => {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers'),
     ])
-      .then(([d, a, i]) => {
-        setState((s) => ({
-          ...s,
-          days: d.data,
-          appointments: a.data,
-          interviewers: i.data,
-        }));
-      })
+      .then(([d, a, i]) => dispatch({ type: 'fetch', args: [d, a, i] }))
       .catch((e) => {
         throw e;
       });
